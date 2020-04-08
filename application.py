77b99d1f820f88d96adb5cd0154b54970c1c9d14
Samanyou Garg <samanyugarg@gmail.com>
2020-04-08 14:12:02 +0100

    Updated pipeline for Beanstalk

---
+++ application.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+
+from app import create_app
+
+
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+if __name__ == "__main__":
+    application.run()
