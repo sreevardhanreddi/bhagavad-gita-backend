cc1a908b8aa4b20522be19d6430e1213f9832da4
Samanyou Garg <samanyugarg@gmail.com>
2020-04-09 09:39:39 +0100

    Added breaking test

--- tests/test_main_blueprint.py
+++ tests/test_main_blueprint.py
@@ -5,4 +5,4 @@ from app.main.views import index
 
 class MainTestCase(unittest.TestCase):
     def test_index(self):
-        assert index() == "RadhaKrishna"
+        assert index() == "RadhaKrishnaHanuman"
