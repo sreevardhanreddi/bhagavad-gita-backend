cf7b982aa835e0f9b0d3765d1e4e3f4996d62c1f
Samanyou Garg <samanyugarg@gmail.com>
2020-04-09 12:17:19 +0100

    Fixed styling

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 
 from app import create_app, db
-from config import Config
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
